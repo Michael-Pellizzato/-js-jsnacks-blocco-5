2d06e63a86d6a1d30caf8b1a252466e55f236905
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-27 12:19:02 +0100

    esercizio 1 correzione

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,8 +1,7 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
-names.forEach(String)
-
-console.log(names)
-
+names.forEach(element => {
+    console.log(element)
+});
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
